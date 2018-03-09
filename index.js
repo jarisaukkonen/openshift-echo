6f5b06dc03de5ab03e2be6745fb9482809e4d9ca
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2018-03-09 10:32:40 +0200

    Add implementation

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.get('*', (req, res) => {
+  data = {
+    "time": new Date(),
+    "method": req.method,
+    "path": req.path,
+    "query": req.query,
+    "headers":req.headers,
+    "body": req.body
+  }
+  console.log(JSON.stringify(data))
+  res.type('json').send(data)
+})
+
+app.listen(8080, () => console.log('echo listening on port 8080'))
