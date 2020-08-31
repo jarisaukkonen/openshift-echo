d60c635e0ae2e24a4af892d23736d9cd9ea2f7f4
Tero Jankkila <tero.jankkila@ambientia.fi>
2020-08-31 10:53:10 +0300

    Response to all request types
    Response to all request types

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env['PORT'] || 8080;
 
-app.get('*', (req, res) => {
+app.all('*', (req, res) => {
   data = {
     '@timestamp': new Date(),
     'level': 'INFO',
